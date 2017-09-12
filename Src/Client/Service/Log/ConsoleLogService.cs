869004c5d3c920b6b94a34424d47676126e5d488
Shane Carvalho <shanec_@hotmail.com>
2017-09-12 19:51:41 +1000

    Added missing log services

---
+++ Src/Client/Service/Log/ConsoleLogService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineManagementApiClient.Service
+{
+    public class ConsoleLogService : ILog
+    {
+        private ILog _log = null;
+
+        public ConsoleLogService()
+        {
+        }
+
+        public ConsoleLogService(ILog log)
+        {
+            this._log = log;
+        }
+
+        public void Debug(string message)
+        {
+            Console.WriteLine($"[Debug]:{message}");
+            _log?.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            Console.WriteLine($"[Error]:{message}");
+            _log?.Error(message);
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            Console.WriteLine($"[Error]:{message}. Exception: /n/tMessage: {exception.Message} StackTrace: {exception.StackTrace}");
+            _log?.Error(message, exception);
+        }
+
+        public void Information(string message)
+        {
+            Console.WriteLine($"[Information]:{message}");
+            _log?.Information(message);
+        }
+    }
+}
---
+++ Src/Client/Service/Log/FileLogService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineManagementApiClient.Service
+{
+    public class FileLogService : ILog
+    {
+        private ILog _log = null;
+        private readonly string _filePath;
+
+        public FileLogService()
+        {
+            this._filePath = "Log.txt";
+        }
+
+        public FileLogService(string filePath)
+        {
+            this._filePath = filePath;
+        }
+
+        public FileLogService(ILog log, string filePath)
+        {
+            this._log = log;
+            this._filePath = filePath;
+        }
+
+        public void Debug(string message)
+        {
+            File.AppendAllLines(_filePath, new[] { $"[Debug]:{message}" });
+            _log?.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            File.AppendAllLines(_filePath, new[] { $"[Error]:{message}" });
+            _log?.Error(message);
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            File.AppendAllLines(_filePath, new[] { $"[Error]:{message}. Exception: /n/tMessage: {exception.Message} StackTrace: {exception.StackTrace}" });
+            _log?.Error(message, exception);
+        }
+
+        public void Information(string message)
+        {
+            File.AppendAllLines(_filePath, new[] { $"[Information]:{message}" });
+            _log?.Information(message);
+        }
+    }
+}
